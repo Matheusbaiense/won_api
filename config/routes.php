e932d045649bd8b000ff845f858feb402b082ded
Seu Nome <seu.email@exemplo.com>
2025-06-04 00:59:47 +0800

    Limpeza Completa - Remoção de 33% dos arquivos inúteis - diagnostic.php, models/, views desnecessárias, config/routes.php - Módulo ultra compacto e eficiente

--- config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$route['won_api/won/api/(:any)'] = 'won/api/$1';
-$route['won_api/won/api/(:any)/(:num)'] = 'won/api/$1/$2';
-$route['won_api/won/join'] = 'won/join';
-$route['won/api/(:any)/(:num)'] = 'won/api/$1/$2';
-$route['won/api/(:any)'] = 'won/api/$1';
